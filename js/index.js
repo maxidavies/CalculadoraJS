2a8904135b0c197252e876139464a3a8e8e49a7c
Maxi Davies <maximilianodavies16@gmail.com>
2021-05-30 01:02:18 -0300

    inicial

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const displayValorAnterior = document.getElementById("valorAnterior");
+const displayValorActual = document.getElementById("valorActual");
+const botonesNumeros = document.querySelectorAll(".numero");
+const botonesOperadores = document.querySelectorAll(".operador");
